933a1726b0c6f8324337667b37ac24ba2f5acbda
NicCOConnor <nic.oconnor@gmail.com>
2015-07-30 16:23:37 -0400

    Add functionality in win_hostname to join a computer to the an Active Directory domain

--- windows/win_hostname.ps1
+++ windows/win_hostname.ps1
@@ -15,22 +15,116 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-
 # WANT_JSON
 # POWERSHELL_COMMON
+Function Required-Args($params,[String[]]$arguments)
+{
+	#Ensure that these arguments are specified
+	$fail = $false
+	$msg = ""
+	foreach($a in $arguments)
+	{
+		if($params.$a -eq $null)
+		{
+			$fail = $true
+			$msg += "$a, "
+		}
+	}
+	if($fail)
+	{
+		$msg += "are required parameters"
+		Fail-Json $msg
+	}
+}
+Function Required-Together($params,[String[]]$arguments)
+{
+	#Ensure that these arguments are specified together
+	$fail = $false
+	$msg = ""
+	foreach ($a in $arguments)
+	{
+		if($params.$a -eq $null)
+		{
+			$fail = $true
+		}
+	}
+	if($fail)
+	{
+		$msg += $arguments -join ":, "
+		$msg += ": are Required Together"
+		Fail-Json $msg
+	}
+}
+Function Required-If($params,$arg,$condition,[String[]]$req_arguments)
+{
+	#Function to conditionally specify parameters If one parameter is specified it's dependencies must also
+	#be specified.
+	$fail = $false
+	$msg = ""
+	if($arg -ne $null)
+	{
+		$arg_check = $true
+		if($arg_check -eq $condition)
+		{
+			foreach($a in $req_arguments)
+			{
+				if($params.$a -eq $null)
+				{
+					$fail = $true
+				}
+			}
+		}
+	}
+	if($fail)
+	{
+		#Get name of passed variable by value
+		$arg_name = ($params.psobject.Properties | where {$_.value -like $arg}).Name
+		
+		$msg += $req_arguments -join ", "
+		$msg += " are required if $arg_name is specified"
+		Fail-Json $msg
+	}
+}
+Function Mutually-Exclusive($params,[String[]]$mutually_exclusive)
+{
+	#Make sure that these arguments are not specified in the same run.
+	$fail = $false
+	$msg = ""
+	$me_count = 0
+	foreach($me in $mutually_exclusive)
+	{
+		if($params.$me -ne $null)
+		{
+			$me_count++
+		}
+	}
+	if($me_count -gt 1)
+	{
+		$fail = $true
+	}
+	if($fail)
+	{
+		$msg += $mutually_exclusive -join ":, "
+		$msg += " Cannot be specified together"
+		Fail-Json $msg
+	}
+}
+
 $params = Parse-Args $args;
 
 $result = New-Object psobject @{
     changed = $false
 }
-If ($params.name)
-{
-	$name = $params.name
-}
-Else
-{
-	Fail-Json $result "missing required argument: name"
-}
+$required = "name"
+$mutuallyExclusive = "domain","workgroup"
+$domain_requiredIf = "domain_user","domain_pass"
+$workgroup_requiredIf = "domain_user","domain_pass"
+
+Required-Args $params $required
+Required-Together $params $requiredTogether
+Mutually-Exclusive $params $mutuallyExclusive
+Required-If $params $params.workgroup $true $workgroup_requiredIf
+Required-If $params $params.domain $true $domain_requiredIf
 
 If($params.restart)
 {
@@ -40,24 +134,80 @@ Else
 {
 	$restart = $false
 }
-#Set the Hostname by way of Powershell module 
+
+$name = $params.name
+$domain = $params.domain
+$domain_user = $params.domain_user
+$domain_pass = $params.domain_pass
+$workgroup = $params.workgroup
+$restart = $params.restart
+
+#Get the hostname and domain
 $curName = [System.Net.Dns]::GetHostName()
-#Make sure the name needs to be changed.
-if ($curName -eq $name)
+$curDomain = (gwmi win32_computersystem).domain
+
+#if domain is specified we use another method to change the name and add it to the domain in one command
+if($domain)
 {
-    $result.changed = $false
+	$changeName = if($curName -ne $name){$true}else{$false}
+	$changeDomain = if($curDomain -ne $domain){$true}else{$false}
+	$netbiosDomain = $domain.split(".")[0]
+	$secPassword = ConvertTo-SecureString $domain_pass -AsPlainText -Force
+	$creds = New-Object System.Management.Automation.PSCredential("$netbiosDomain\$domain_user",$secPassword)
+
+	if($changeName -and $changeDomain)
+	{
+		try
+		{
+			Rename-Computer -NewName $name -Force
+			Add-Computer -Credential $creds -DomainName $domain -PassThru -ErrorAction Stop -Options AccountCreate,JoinWithNewName
+		}
+		catch
+		{
+			Fail-Json "$Error.Exception"
+		}
+		$result.changed = $true
+	}
+	elseif($changedomain)
+	{
+	try
+		{
+			Add-Computer -Credential $creds -DomainName $domain -PassThru -ErrorAction Stop
+		}
+		catch
+		{
+			Fail-Json "$Error.Exception"
+		}
+		$result.changed = $true
+	}
 }
-Else
-{ 
-    If ($restart)
-    {
-        Rename-Computer -NewName $name -Restart -Force
-    }
-    Else
-    {
-        Rename-Computer -NewName $name
-    }
-    $result.changed = $true
+elseif($workgroup)
+{
+	$secPassword = ConvertTo-SecureString $domain_pass -AsPlainText -Force
+	$netbiosDomain = $curDomain.split(".")[0]
+	$creds = New-Object System.Management.Automation.PSCredential("$netbiosDomain\$domain_user",$secPassword)
+	try
+	{
+		Remove-Computer -UnjoinDomainCredential $creds -Force -PassThru -ErrorAction Stop
+	}
+	catch
+	{
+		Fail-Json "$Error.Exception"
+	}
+	$result.changed = $true
+}
+# Just Change the Hostname
+else
+{
+	if($curName -ne $name)
+	{
+		Rename-Computer -NewName $name -Force
+    	$result.changed = $true
+	}
+}
+#only reboot if the machine has been changed.
+if(($restart) -and ($result.changed))
+{
+	Restart-Computer -Force
 }
 Exit-Json $result
-
--- windows/win_hostname.py
+++ windows/win_hostname.py
@@ -3,8 +3,8 @@
 DOCUMENTATION = '''
 ---
 module: win_hostname
-version_added: "1.8"
-short_description: Changes the hostname of a windows machine
+version_added: "2.0"
+short_description: Changes the hostname of a windows machine optionally adds or removes from a domain
 description:
 	- Changes the Hostname of a windows machine
 options:
@@ -16,13 +16,39 @@ options:
 	  aliases: []
   restart:
     description:
-	  - Restart after name change
+	  - Restart when done.
 	required: false
 	choices:
 	- yes
 	- no
 	default: null
 	aliases: []
+  domain:
+    description:
+      - When specified will join a computer to the domain, requires domain_user and domain_pass
+    required: false
+    default: null
+    aliases: []
+  workgroup:
+    description:
+    - When specified will change the workgroup of a computer, if added to a domain it will remove the 
+      computer from the domain, cannot be specified with domain
+    required: false
+    default: null
+    aliases: []
+  domain_user:
+    description:
+      - Domain user that has permissions to add computers to the domain, required if domain or workgroup is specified
+    required: false
+    default: null
+    aliases: []
+  domain_pass:
+    description:
+      - Password for domain_user, required if domain or workgroup is specified
+    required: false
+    default: null
+    aliases: []
+  
 author: Nic O'Connor
 '''
 
@@ -45,4 +71,19 @@ $ansible -i hosts -m win_hostname -a "name=test restart=yes" windows
       win_hostname:
         name: "{{inventory_hostname_short}}"
         restart: yes
+        
+- name: Change Hostname and add to domain
+  win_hostname:
+    name: "{{inventory_hostname_short}}"
+    domain: "testing.com"
+    domain_user: Administrator
+    domain_pass: testing123
+    restart: yes
+
+- name: Remove Computer from domain
+    name: {{inventory_hostname_short}}
+    workgroup: WORKGROUP
+    domain_user: Administrator
+    domain_pass: testing123
+    restart: yes
 '''
